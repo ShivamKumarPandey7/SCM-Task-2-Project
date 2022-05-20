b26eacdd485726cfaa93486c6a6870d31978f35b
ShivamKumarPandey7 <101385500+ShivamKumarPandey7@users.noreply.github.com>
2022-05-20 05:46:14 +0530

    Add files via upload
    Added through drag and drop

--- firstpage.js
+++ firstpage.js
@@ -4,6 +4,7 @@ let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
 let endDate = new Date(2023, 0, 1, 00, 00);
 let endTime = endDate.getTime();
+
 function countdown() {
   let todayDate = new Date();
   let todayTime = todayDate.getTime();
@@ -13,7 +14,8 @@ function countdown() {
   let oneDay = 24 * oneHr;
 
   let addZeroes = (num) => (num < 10 ? `0${num}` : num);
-if (endTime < todayTime) {
+
+  if (endTime < todayTime) {
     clearInterval(i);
     document.querySelector(
       ".countdown"
@@ -23,7 +25,8 @@ if (endTime < todayTime) {
     let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
     let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
     let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
- dayBox.textContent = addZeroes(daysLeft);
+
+    dayBox.textContent = addZeroes(daysLeft);
     hrBox.textContent = addZeroes(hrsLeft);
     minBox.textContent = addZeroes(minsLeft);
     secBox.textContent = addZeroes(secsLeft);
